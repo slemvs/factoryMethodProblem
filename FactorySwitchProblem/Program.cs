bec10a5201f5ccfb24a3c78cc1ff69b6cc1d0596
Semyon Lopatkin <s.lopatkin@eastbanctech.ru>
2018-05-14 01:08:58 +0700

    Add project files.

---
+++ FactorySwitchProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactorySwitchProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
